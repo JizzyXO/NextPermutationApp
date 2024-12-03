973f47814ac2ccc8f6c2fceac10dc347ac990c0c
pavel <pavel@JIZZY>
2024-12-03 15:50:54 +0300

    Добавьте файлы проекта.

---
+++ NextPermutationApp/NextPermutationTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NextPermutationApp;
+
+namespace NextPermutationTests
+{
+    [TestClass]
+    public class NextPermutationTests
+    {
+        [TestMethod]
+        public void TestNextPermutation()
+        {
+            int[] nums = { 1, 2, 3 };
+            Program.NextPermutation(nums);
+            CollectionAssert.AreEqual(new int[] { 1, 3, 2 }, nums);
+        }
+
+        [TestMethod]
+        public void TestNextPermutationLast()
+        {
+            int[] nums = { 3, 2, 1 };
+            Program.NextPermutation(nums);
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3 }, nums);
+        }
+
+        [TestMethod]
+        public void TestNextPermutationMiddle()
+        {
+            int[] nums = { 1, 3, 2 };
+            Program.NextPermutation(nums);
+            CollectionAssert.AreEqual(new int[] { 2, 1, 3 }, nums);
+        }
+    }
+}
---
+++ NextPermutationApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NextPermutationApp
+{
+    public class Program
+    {
+        public static void NextPermutation(int[] nums)
+        {
+            int i = nums.Length - 2;
+            while (i >= 0 && nums[i] >= nums[i + 1])
+            {
+                i--;
+            }
+
+            if (i >= 0)
+            {
+                int j = nums.Length - 1;
+                while (nums[j] <= nums[i])
+                {
+                    j--;
+                }
+                Swap(nums, i, j);
+            }
+            Array.Reverse(nums, i + 1, nums.Length - i - 1);
+        }
+
+        private static void Swap(int[] nums, int i, int j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3 };
+            NextPermutation(nums);
+            Console.WriteLine(string.Join(", ", nums)); // Output: 1, 3, 2
+        }
+    }
+}
